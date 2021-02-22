10b61da969d674ce9c460b2f6fa49487d6e6f2d6
seema thorat <seemagade97@gmail.com>
2021-02-22 15:35:06 +0530

    Check Option no play,snake and ladder

--- LadderAndSnake.java
+++ LadderAndSnake.java
@@ -1,17 +1,38 @@
 import java.util.*;
 
-public class LadderAndSnake
+class LadderAndSnake{
 
-{
-     public static void main(String args[])
+    public static void main(String[] args){
 
-  {
+      int position=0;
+      System.out.println("Start with position "+position);
       Random r=new Random();
+      int x=r.nextInt(7);
+      System.out.println("Rolling the dice " +x);
 
-     int random=r.nextInt(6);
+      Random r1=new Random();
+      int y=r1.nextInt(3);
+      System.out.println("Steps" +y);
 
-     System.out.println("Randomely generated dice "+random);
-}
+      if(y==0)
+      {
+         System.out.println("Player is in same position....");
+      }
+      else if(y==1)
+      {
+         System.out.println("Player move behind");
+         position=position-x;
+         System.out.println("Current position of player is:"+position);
+      }
+      else if(y==2)
+      {
+         System.out.println("Player Move ahead");
+         position=position-x;
+         System.out.println("Current position of the player is:"+position);
+      }
+
+   }
+
 }
 
 
